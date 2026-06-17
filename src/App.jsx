192c263a253818baf06b5d949eac8cfcd0e05c8a
Monica Ashmitha <monicaashmithakasarapu08@gmail.com>
2026-06-17 10:46:03 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Pharmacy Credits Due Tracker</h1>
+      <p>Welcome to your tracking app!</p>
+    </div>
+  );
+}
+
+export default App;
