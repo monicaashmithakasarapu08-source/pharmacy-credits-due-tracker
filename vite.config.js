5b7498205c5edef5a88ca602f46cf67ed3b1c761
Monica Ashmitha <monicaashmithakasarapu08@gmail.com>
2026-06-17 10:38:16 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,1 @@
+import { defineConfig } from 'vite';\nimport react from '@vitejs/plugin-react';\n\n// https://vitejs.dev/config/\nexport default defineConfig({\n  plugins: [react()],\n  server: {\n    port: 3000,\n    open: false,\n    host: true,\n  },\n  build: {\n    outDir: 'dist',\n    sourcemap: false,\n  },\n});
